62acb5781a6a3b671311bb7af5fcdff84facd8b1
Jan Kujawa <jan@isbroken.com>
2024-03-02 16:27:12 -0700

    Adds matrix formatting and rotation.

--- linear.py
+++ linear.py
@@ -27,11 +27,19 @@ def fv(v):
     core = [f"{float(f):.3f}" for f in v]
     return f"[{' '.join(core)}]"
 
+def fm(m):
+    # format matrix
+    return fm_(m)
+
+def matmul(a, b):
+    return matmul_(a, b)  
+      
 Matrix = None
 cross = None
        
 try:
     import numpy as np
+    import math as m
     print("Numpy detected")
     def vector_(args):
         return np.array(args)
@@ -42,6 +50,11 @@ try:
     dot_ = lambda a, b: np.dot(a, b)
     vmag_ = lambda v: np.linalg.norm(v)
     normalize_ = lambda v: v/np.linalg.norm(v)
+    matmul_ = lambda a, b: np.matmul(a, b)
+    def fm_(m):
+        core = [fv(v) for v in m] 
+        nl = '\n'
+        return f"[{nl.join(core)}]"
     #def testprec_():
     #    np.set_printoptions(precision = 3, floatmode="fixed")
 except Exception as e:
@@ -53,7 +66,7 @@ except Exception as e:
         import umath as m
         def vector_(args):
             return pbvector(*args)
-        #print("pybricks detected")
+        print("PyBricks detected")
         vlen_ = lambda v: v.shape[0]
         Matrix = pbmatrix
         cross = pbcross
@@ -62,16 +75,48 @@ except Exception as e:
         def vmag_(v):
             return m.sqrt(dot(v, v))
         normalize_ = lambda v: v/vmag(v)
+        matmul_ = lambda a, b: a*b
+        def fm_(m):
+            rows = [ [m[3*z], m[3*z+1], m[3*z+2]] for z in range(3)]
+            core = [fv(v) for v in rows] 
+            nl = '\n'
+            return f"[{nl.join(core)}]"
+            #return fv(m)
     except Exception as e:
         print(f"linear algebra not detected: {e}")
         raise
- 
+
+# https://stackoverflow.com/questions/6802577/rotation-of-3d-vector
+xaxis = vector(1, 0, 0)
+yaxis = vector(0, 1, 0)
+zaxis = vector(0, 0, 1)
+def rmat(axis, theta):
+    """
+    Return the rotation matrix associated with counterclockwise rotation about
+    the given axis by theta radians.
+    """
+    axis = normalize(axis)
+    a = m.cos(theta / 2.0)
+    b, c, d = -axis * m.sin(theta / 2.0)
+    aa, bb, cc, dd = a * a, b * b, c * c, d * d
+    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+    return Matrix([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
+
+def rotate(axis, theta, point):
+    rm = rmat(axis, theta)
+    return np.matmul(rm, point)
+     
 def test():
     v = vector(1.0, 2.0, 3.0)
     w = vector(3, -2, 9)
     u = cross(v, w)
     print(f"fv: {fv(v)} v.shape[0]: {v.shape[0]}, v[0]: {v[0]} vlen(v): {vlen(v)}")
     print(f"vmag(v): {vmag(v):.3f} fw: {fv(w)} fu: {fv(u)} normalize(u): {fv(normalize(u))}")
+    xaxis45 = rmat(xaxis, m.radians(45))
+    rotv = matmul(xaxis45, v)
+    print(f"xaxis45: {fm(xaxis45)} fv(rotv): {fv(rotv)}")
            
 if __name__ == "__main__":
     if(vector):
