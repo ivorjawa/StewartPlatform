a6d86cde4adb8efba0cd6d010df1c869e64e9f6d
Jan Kujawa <jan@isbroken.com>
2024-03-04 11:04:06 -0700

    Objectifies base station.

--- rotorbase.py
+++ rotorbase.py
@@ -5,16 +5,13 @@ import datetime, time
 import logging, logging.handlers
 from asyncio import gather, sleep, run
 
-import bleak
-
 from pybricksdev.connections.pybricks import PybricksHub
 from pybricksdev.ble import find_device, nus
+import bleak
 
 import controllers as ctrl
 
 logging.basicConfig(level=logging.INFO, format='%(message)s [%(levelname)s:%(name)s]')
-
-gamepad = ctrl.TaranisX9d()
 
 class  LoggingBricksHub(PybricksHub):
     """
@@ -51,47 +48,56 @@ class  LoggingBricksHub(PybricksHub):
         except Exception as e:
             logging.error(f"_line_handler error: {e}")
         
-last_sent = time.time()
-async def send_data(hub):
-    global last_sent 
-    lastout = ""
+class BaseStation(object):
+    def __init__(self):
+        self.last_sent = time.time()
+        self.gamepad = ctrl.TaranisX9d()
+    async def send_data(self, hub):
+        global last_sent 
+        lastout = ""
 
-    logging.info("starting main loop")
-    while 1:
+        logging.info("starting main loop")
+        while 1:
         
-        report = gamepad.report()
-        if report:            
-            coll = report['coll'] 
-            roll = report['roll']          
-            pitch = report['pitch']
-            yaw = report['yaw']
-            glyph = report['glyph']
+            report = self.gamepad.report()
+            if report:            
+                coll = report['coll'] 
+                roll = report['roll']          
+                pitch = report['pitch']
+                yaw = report['yaw']
+                glyph = report['glyph']
             
-            output = ">c%03xr%03xp%03xy%03xg%03x<" % (coll, roll, pitch, yaw, glyph)
-            if (output != lastout) or (((time.time()-last_sent)*1000) > 16):
-                lastout = output
-                logging.info(output)
-                try:
-                    await hub.write(bytearray(output, 'ascii'))
-                    last_sent = time.time()
-                except bleak.exc.BleakError as e:
-                    logging.debug(f"BLE communication error: {e}")
-                except Exception as e:
-                    logging.error(f"Other error in hub.write(): {e}")
-                    #sys.exit(0)
+                output = ">c%03xr%03xp%03xy%03xg%03x<" % (coll, roll, pitch, yaw, glyph)
+                if (output != lastout) or (((time.time()-self.last_sent)*1000) > 16):
+                    lastout = output
+                    logging.info(output)
+                    try:
+                        await hub.write(bytearray(output, 'ascii'))
+                        self.last_sent = time.time()
+                    except bleak.exc.BleakError as e:
+                        logging.debug(f"BLE communication error: {e}")
+                    except Exception as e:
+                        logging.error(f"Other error in hub.write(): {e}")
+                        #sys.exit(0)
     
-async def go():
-    hub = LoggingBricksHub('rotorbase')
-    address = await find_device('rotor')
-    await hub.connect(address)   
+    async def go(self):
+        hub = LoggingBricksHub('rotorbase')
+        address = await find_device('rotor')
+        await hub.connect(address)   
 
-    try:
-        await hub.run('rotor.py', wait=False)
-        await gather(send_data(hub))
+        try:
+            await hub.run('rotor.py', wait=False)
+            await gather(self.send_data(hub))
 
-    except Exception as e:
-        logging.error("script gather failed:  ", e)
+        except Exception as e:
+            logging.error("script gather failed:  ", e)
 
-    await hub.disconnect()
-
-run(go())
+        await hub.disconnect()
+    
+    def engage(self):
+        run(self.go())
+        
+if __name__ == "__main__":
+    rotor = BaseStation()
+    rotor.engage() # make it so
+    
