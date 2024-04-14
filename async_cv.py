7690f0b09bc06cc211b3f02559817fb202064dc1
Jan Kujawa <jan@isbroken.com>
2024-04-14 15:20:34 -0600

    It's doing it in video.

--- async_cv.py
+++ async_cv.py
@@ -8,14 +8,21 @@ import functools
 from concurrent.futures import ProcessPoolExecutor
 executor = ProcessPoolExecutor(1)
 
+cam = cv2.VideoCapture("/Users/kujawa/Desktop/color_ring_crop.mov")
+
 def load_img(image):
     print(image)
-    im = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
-    _, inv = cv2.threshold(im, 150, 255, cv2.THRESH_BINARY_INV)
-    cv2.GaussianBlur(inv, (3, 3), 0)
-    cv2.imshow('Async test', inv)
-    #cv2.waitKey(0)
-    cv2.pollKey()
+    #im = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+    #_, inv = cv2.threshold(im, 150, 255, cv2.THRESH_BINARY_INV)
+    #cv2.GaussianBlur(inv, (3, 3), 0)
+    while 1:
+        ret, frame = cam.read()
+        if not ret:
+            break
+        cv2.imshow('Async test', frame)
+        #cv2.waitKey(0)
+        if cv2.pollKey() == 27:
+            break
     cv2.destroyAllWindows()
     return
 
